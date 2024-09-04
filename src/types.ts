585cd26401eca595eb2a0c9fa0394a0f2cc183bd
Antonio0806 <antonio0806123@gmail.com>
2024-09-04 21:35:25 +0200

    Revert "Revert "The environementCreate call responds with the containers data.""
    This reverts commit 0eef58547b3c1d34e688f603725bada2a7d728db.

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,17 @@
 import { DateTime, Str } from "@cloudflare/itty-router-openapi";
+import { Container, IpAddress, Volume } from "@azure/arm-containerinstance";
+import { kStringMaxLength } from "buffer";
 
+export type EnvironementAzureResonseReassignement = {
+	containers: Container[] | undefined,
+	ipAdress: IpAddress | undefined,
+	location: string | undefined,
+	id: string | undefined,
+	name: string | undefined,
+	osType: string | undefined,
+	provisioningState: string | undefined,
+	volumes: Volume[] | undefined,
+};
 
 export const EnvironementType = {
 	containers: [{
